1c61f1bfd800cfc3ce10bd8262d8c4575be3ef87
Muhammed Athimannil <hello@athimannil.com>
2017-07-31 00:29:55 +0200

    Header statless component

---
+++ src/app/components/header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<header>
+			<div className="container">
+				<h3>Latest properties</h3>
+			</div>
+		</header>
+	);
+};
