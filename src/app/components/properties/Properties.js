3c96ca5306c1119329f1c7d1b109fbe4192960e8
Muhammed Athimannil <hello@athimannil.com>
2017-07-31 01:16:51 +0200

    desktop design

--- src/app/components/properties/Properties.js
+++ src/app/components/properties/Properties.js
@@ -24,6 +24,145 @@ export class Properties extends React.Component {
 							</div>
 						</article>
 
+
+
+
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+						<article className="property">
+							<figure className="property-image">
+								<img src="http://via.placeholder.com/500x300" />
+								<figcaption className="property-image-title">mage title</figcaption>
+							</figure>
+							<div className="property-detail">
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
+								</div>
+							</div>
+						</article>
+
 					</div>
 				</div>
 			</main>
