19b5de9074ef8fa84e07352752f7b27e5fe0e3f5
Muhammed Athimannil <hello@athimannil.com>
2017-07-31 01:13:41 +0200

    property address style

--- src/app/components/properties/Properties.js
+++ src/app/components/properties/Properties.js
@@ -14,12 +14,12 @@ export class Properties extends React.Component {
 								<figcaption className="property-image-title">mage title</figcaption>
 							</figure>
 							<div className="property-detail">
-								<h3>property title</h3>
-								<h6>Property address</h6>
-								<div className="">
-									<div>110.00¢</div>
-									<div>3 Zimmer</div>
-									<div>ab 35m²</div>
+								<h3 className="property-detail-title">property title</h3>
+								<h6 className="property-detail-address">Property address</h6>
+								<div className="property-detail-size">
+									<div className="property-detail-amount">110.00¢</div>
+									<div className="property-detail-room">3 Zimmer</div>
+									<div className="property-detail-space">ab 35m²</div>
 								</div>
 							</div>
 						</article>
