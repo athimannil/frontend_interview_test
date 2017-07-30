5939e5e766559c7283e86ca9a9835cb676ae9f8f
Muhammed Athimannil <hello@athimannil.com>
2017-07-31 00:47:23 +0200

    properties as new component

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react';
 
 import { Header } from './components/header/Header';
+import { Properties } from './components/properties/Properties';
 import { Footer } from './components/footer/Footer';
 
 class App extends React.Component {
@@ -10,11 +11,7 @@ class App extends React.Component {
 		return (
 			<div className="app-wrap">
 				<Header />
-				<main>
-					<div className="container">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					</div>
-				</main>
+				<Properties />
 				<Footer />
 			</div>
 		);
