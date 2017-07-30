acc1d559b76a75f8cad56495ffe4a43b55356fa9
Muhammed Athimannil <hello@athimannil.com>
2017-07-31 00:35:47 +0200

    Footer statless component

--- src/app/App.js
+++ src/app/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import React from 'react';
 
 import { Header } from './components/header/Header';
+import { Footer } from './components/footer/Footer';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="app-wrap">
-				<Header/>
+				<Header />
 				<main>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</main>
+				<Footer />
 			</div>
 		);
 	}
---
+++ src/app/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+import React from 'react';
+
+export const Footer = () => {
+	return(
+		<footer>
+			<div className="container">
+				<p>Flexbox Tickets 2017</p>
+			</div>
+		</footer>
+	);
+};
